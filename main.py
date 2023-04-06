5ef2860d3cffd36b5046930557aa1ebd3b1ed5f0
Ram <59142683+RamSwami0021@users.noreply.github.com>
2023-04-07 03:36:00 +0530

    ok
    Signed-off-by: Ram <59142683+RamSwami0021@users.noreply.github.com>

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ worksheet = workbook.active
 model = predict.load_model("./models/keras_nsfw_mobilenet2.224x224.h5")
 
 # this code for online test data use
-number = int(input("Enter the no of img (1 to 1000) :- "))
-for i in range(1, number):
+for i in range(1, 1000):
     urllib.request.urlretrieve("https://github.com/RamSwami0021/NSFW_Detector/blob/main/test_data/{i}.jpg", "image_{i}.jpg")
     result = predict.classify(model, "image_{i}.jpg")
     worksheet.append(result)
