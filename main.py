06443e41c34f4ed593c3601a9b10836953f31744
Ram Swami <ramswami@Rams-MacBook-Air.local>
2023-04-07 03:40:07 +0530

    ok

---
+++ main.py
@@ -0,0 +1,23 @@
+from nsfw_detector import predict
+import openpyxl
+import urllib.request
+
+workbook = openpyxl.Workbook()
+worksheet = workbook.active
+
+model = predict.load_model("./models/keras_nsfw_mobilenet2.224x224.h5")
+
+# this code for online test data use
+# number = int(input("Enter the no of img (1 to 1000) :- "))
+# for i in range(1, number):
+#     urllib.request.urlretrieve("https://github.com/RamSwami0021/NSFW_Detector/blob/main/test_data/{i}.jpg", "image_{i}.jpg")
+#     result = predict.classify(model, "image_{i}.jpg")
+#     worksheet.append(result)
+
+# this code for offline test data use
+number = int(input("Enter the no of img (1 to 1000) :- "))
+for i in range(1, number):
+    result = predict.classify(model, "./test_data/{i}.jpg")
+    worksheet.append(result)
+
+workbook.save('Final_Result.xlsx')
