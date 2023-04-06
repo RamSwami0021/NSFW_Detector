5b46495ea4c108ce90024a6cceeb520a4ecef7ad
Ram <59142683+RamSwami0021@users.noreply.github.com>
2023-04-07 03:40:18 +0530

    ok
    Signed-off-by: Ram <59142683+RamSwami0021@users.noreply.github.com>

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ model = predict.load_model("./models/keras_nsfw_mobilenet2.224x224.h5")
 #     worksheet.append(result)
 
 # this code for offline test data use
-number = int(input("Enter the no of img (1 to 1000) :- "))
-for i in range(1, number):
+
+for i in range(1, 1000):
     result = predict.classify(model, "./test_data/{i}.jpg")
     worksheet.append(result)
 
